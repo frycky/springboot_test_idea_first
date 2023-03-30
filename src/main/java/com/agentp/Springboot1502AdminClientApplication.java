5883846c037a6d7341b9d0102987be460b1e3f02
Agentp <2569787219@qq.com>
2023-03-30 18:09:53 +0800

    idea_VCS_test commit

---
+++ src/main/java/com/agentp/Springboot1502AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.agentp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1502AdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot1502AdminClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agentp/Springboot1502AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1502AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
